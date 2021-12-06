1fa668b98fe5cd3e0195c2743fa173415f12ce09
SirawitN <nine_sirawit@hotmail.com>
2021-12-06 20:47:32 +0700

    logIn_system_added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,56 @@
 class MainController < ApplicationController
 
 	def logIn
+		puts "user_id = #{session[:user_id]}, store_id = #{session[:store_id]}"
+		session[:user_id] = nil
+		session[:store_id] = nil
 		@user = User.new
+	end
+
+	def cust_login
+		@user = User.new
+	end
+
+	def store_login
+		@store = Store.new
 	end
 
 	def register
 		@user = User.new
 	end
+
+	def findCust
+		puts "username, pass = #{params["user"]["username"]}, #{params["user"]["password"]}"
+		@user = User.find_by(username: params["user"]["username"]).authenticate(params["user"]["password"]) rescue nil
+	    respond_to do |format|
+	      	if @user
+	            puts "Found"
+	            format.html {redirect_to users_url}
+	            session[:user_id] = @user.id
+	        else 
+	            puts "Not found"
+	            flash.alert = "Log in failed, wrong email or password !!"
+	            format.html { redirect_to index_path}    
+	        end
+	    end
+	end
+
+
+	def findStore
+		puts "username, pass = #{params["store"]["username"]}, #{params["store"]["password"]}"
+		@store = Store.find_by(username: params["store"]["username"]).authenticate(params["store"]["password"]) rescue nil
+	    respond_to do |format|
+	      	if @store
+	            puts "Found"
+	            format.html {redirect_to stores_url}
+	            session[:store_id] = @store.id
+	        else 
+	            puts "Not found"
+	            flash.alert = "Log in failed, wrong email or password !!"
+	            format.html { redirect_to index_path}    
+	        end
+	    end
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :stores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/main', to: "main#logIn", as: "logIn"
+  get '/main', to: "main#logIn", as: "index"
+
+  get '/main/customer', to: 'main#cust_login', as: "cust_login"
+  get '/main/store', to: 'main#store_login', as: 'store_login'
+  post '/main/customer', to: 'main#findCust', as: 'find_cust'
+  post '/main/store', to: 'main#findStore', as: 'find_store'
+
   get '/main/register', to: "main#register", as: "register"
 end
